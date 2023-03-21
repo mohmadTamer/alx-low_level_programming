c123738c5ee01d3ab5e78a311ab75cdce3e46378
mohamed <m.tamer3997@gmail.com>
2023-03-20 19:11:08 -0700

    0x00-hello_world

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main (void)
     else if (n>0)
         printf("%d is positive\n", n);
     else
-        printf("%d is Zero\n", n);
+        printf("%i is zero\n", n);
     
     return (0);
 }
